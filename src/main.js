b9f231e34f93c5c66232cc0248115a5e8b9b07ca
kejianlin <13163258663@163.com>
2019-09-09 00:11:32 +0800

    项目第一天

--- src/main.js
+++ src/main.js
@@ -1,14 +1,28 @@
 import Vue from 'vue'
 
-import MintUI from 'mint-ui'
+import VueRouter from 'vue-router'
 
-import 'mint-ui/lib/style.css'
-import './lib/mui/css/mui.css'
-import app from './App.vue'
+Vue.use(VueRouter)
+
+import VueResource from 'vue-resource'
+
+Vue.use(VueResource)
+
+import MintUI from 'mint-ui'
 
 Vue.use(MintUI)
 
+import 'mint-ui/lib/style.css'
+import './lib/mui/css/mui.css'
+import './lib/mui/css/icons-extra.css'
+import app from './App.vue'
+
+
+
+import router from './router.js'
+
 new Vue({
   el: '#app',
-  render: c => c (app)
+  render: c => c (app),
+  router
 })
--- src/router.js
+++ src/router.js
@@ -1,14 +1,25 @@
 import VueRouter from 'vue-router'
 
-import account from './main/Account.vue'
-import goodslist from './main/GoodsList.vue'
 
-import login from './subcom/login.vue'
-import register from './subcom/register.vue'
+import HomeContainer from './components/tabbar/HomeContainer.vue'
+import MemberContainer from './components/tabbar/MemberContainer.vue'
+import ShopcarContainer from './components/tabbar/ShopcarContainer.vue'
+import SearchContainer from './components/tabbar/SearchContainer.vue'
+
+// import account from './main/Account.vue'
+// import goodslist from './main/GoodsList.vue'
+// import login from './subcom/login.vue'
+// import register from './subcom/register.vue'
 
 var router = new VueRouter({
   routes:[
-  ]
+    {path:"/", redirect:'/home'},
+    {path:'/home', component:HomeContainer},
+    {path:'/member', component:MemberContainer},
+    {path:'/shopcar', component:ShopcarContainer},
+    {path:'/search', component:SearchContainer}
+  ],
+  linkActiveClass:"mui-active"
 })
 
 export default router
